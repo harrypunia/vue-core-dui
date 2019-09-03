5bb4b88ebbda95cff182763801cb488131ce3844
Harry <harryrock67@gmail.com>
2019-09-03 15:06:45 -0400

    Created Input - Added Label, input and underline - Added Fexlible css props - Added Prop validation - Added two way data-binding with sync - Added onChange fallback

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div style="width: 250px; margin: 20px">
+      <p>Data Binding: {{input}}</p>
+      <Input name="input" :value.sync="input" label="Input"/>
+      <Input name="custom-css" :value.sync="customInput" label="Custom CSS" :css="inputCSS"/>
+    </div>
+  </div>
 </template>
 
 <script>
+  import Input from "../src/components/input/Input";
+
   export default {
-    name: 'app'
+    name: 'app',
+    components: {Input},
+    data() {
+      return {
+        input: "Input",
+        customInput: "Custom Input",
+        inputCSS: {
+          container: {
+            background: "#f7f7f7",
+            margin: "20px 0"
+          },
+          underline: {
+            backgroundColor: "green",
+          },
+          activeUnderline: {
+            backgroundColor: "purple"
+          }
+        }
+      }
+    }
   }
 </script>
 
-<style></style>
+<style>
+  html, body {
+    font-family: sans-serif;
+  }
+</style>
---
+++ src/components/input/Input.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="dui-input-container" :style="css ? (css.container || {}) : {}">
+    <input
+        :name="name" :type="type || 'text'"
+        class="dui-input"
+        v-model="value_"
+        :style="css ? (css.input || {}) : {}"
+        @input="onChange ? onChange(value_) : onInput()"
+    />
+    <label
+        :for="name"
+        class="dui-input-label"
+        :class="{'dui-input-label-active' : value}"
+        :style="css ? (css.label || {}) : {}">
+      {{label}}
+    </label>
+    <div class="dui-input-underline" :style="css ? (css.underline || {}) : {}"></div>
+    <div class="dui-input-underline-active" :style="css ? (css.activeUnderline || {}) : {}"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Input",
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      value: {
+        type: String,
+        required: true
+      },
+      type: String,
+      label: String,
+      onChange: Function,
+      css: Object
+    },
+    data() {
+      return {
+        value_: this.value
+      }
+    },
+    methods: {
+      onInput() {
+        this.$emit('update:value', this.value_);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>@import "../resources/scss/input/input";</style>
